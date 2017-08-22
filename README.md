40d6f5986e6f5fc9c8f121dfb9473d13e3cdaaf5
Vadzim Tarasiuk <vad.tarasyuk@gmail.com>
2017-08-22 16:31:55 +0300

    Tests

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Installation community versions of Jenkins:
 
 <img src="1-1.png">
+
+## Test verifying:
+
+<img src="1-2.png">
--- task1_community/test/smoke/default/default_test.rb
+++ task1_community/test/smoke/default/default_test.rb
@@ -9,15 +9,15 @@ describe package('jenkins') do
   it { should be_installed }
 end
 
-describe package('java') do
-  it { should be_installed }
-end
-
 describe service('jenkins') do
   it { should be_enabled }
   it { should be_running }
 end
 
-describe command('curl -IL localhost:8080/') do
+describe command('curl -IL localhost:8080') do
   its('stdout') { should match /200 OK/ }
+end
+
+describe command('curl -IL localhost:8080') do
+  its('stdout') { should match /X-Jenkins:/ }
 end
